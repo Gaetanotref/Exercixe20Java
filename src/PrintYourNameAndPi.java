b782ef18af40b2d9147adfef8442c43954e29d45
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-07 16:34:33 +0100

    Commit init

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,6 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name="Gaetano";double pi = 3.141590;
+        System.out.printf("%s,\nThe number Pi is: %f",name,pi);
+    }
+}
